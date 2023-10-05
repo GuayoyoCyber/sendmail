a4755ef9d8f7dcd8cbe9215eb248ff2fabb1a2fc
Bl4ckMatt <56125993+Bl4ckMatt@users.noreply.github.com>
2023-10-05 10:15:42 -0300

    Added exception if file does not exists

--- sendMail.py
+++ sendMail.py
@@ -1,6 +1,7 @@
 import base64
 import os
 import json
+import sys
 from sendgrid import SendGridAPIClient
 from sendgrid.helpers.mail import (
     Mail, Attachment, FileContent, FileName,
@@ -23,32 +24,38 @@ atchName = os.environ['ATCH_NAME']
 atchId = os.environ['ATCH_ID']
 sendgridApiKey= os.environ['SENDGRID_API_KEY']
 
-message = Mail(
+if os.path.exists(filePath):
+    message = Mail(
     from_email=fromaddr,
     to_emails=toaddr,
     subject=subject,
     html_content=body)
     
-with open(filePath, 'rb') as f:
-    data = f.read()
-    f.close()
+    with open(filePath, 'rb') as f:
+        data = f.read()
+        f.close()
+    
+    encoded = base64.b64encode(data).decode()
+    attachment = Attachment()
+    attachment.file_content = FileContent(encoded)
+    attachment.file_type = FileType(atchType)
+    attachment.file_name = FileName(atchName)
+    attachment.disposition = Disposition('attachment')
+    attachment.content_id = ContentId(atchId)
+    message.attachment = attachment
+    
+    try:
+        sendgrid_client = SendGridAPIClient(sendgridApiKey)
+        response = sendgrid_client.send(message)
+        print('Mail sent Succesfully')
+        print('Response: '+str(response.status_code))
+        print('Response body: '+str(response.body))
+        print('Response Headers: '+str(response.headers))
+    except Exception as e:
+        print('Error while sending Email, see exception below: ')
+        print(e.message)
+else:
+    print(f"File {filePath} does not exists")
+    sys.exit(1)
 
-encoded = base64.b64encode(data).decode()
-attachment = Attachment()
-attachment.file_content = FileContent(encoded)
-attachment.file_type = FileType(atchType)
-attachment.file_name = FileName(atchName)
-attachment.disposition = Disposition('attachment')
-attachment.content_id = ContentId(atchId)
-message.attachment = attachment
 
-try:
-    sendgrid_client = SendGridAPIClient(sendgridApiKey)
-    response = sendgrid_client.send(message)
-    print('Mail sent Succesfully')
-    print('Response: '+str(response.status_code))
-    print('Response body: '+str(response.body))
-    print('Response Headers: '+str(response.headers))
-except Exception as e:
-    print('Error while sending Email, see exception below: ')
-    print(e.message)
